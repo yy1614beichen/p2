c26c4fc1b489891f8b23784117629e241e7b0074
root <13995624101@163.com>
2018-12-17 10:48:06 +0800

    将Java工程推送到远程仓库了

---
+++ src/com/itheima/git/FirstGit.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("春眠不觉晓");
+        System.out.println("处处受骚扰");
+        System.out.println("夜来哞床声");
+        System.out.println("少女变大嫂");
+
+    }
+}
